56afecdbaa73ef5b636b9e8c92a70334219aebb7
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-11-19 20:25:26 +0100

    migrate links tags

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
+    pool: {
+        afterCreate:(conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
+    },
     useNullAsDefault: true,
     migrations: {
         directory: path.resolve(__dirname, "src", "database", "knex", "migrations")

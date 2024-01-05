8c64f5668efe0ede3d4983b4ea8dbfb14ca051dc
Fernando Rodrigues <fernando111oficina@gmail.com>
2024-01-05 12:54:15 +0100

    sessions

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,8 +1,24 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
+
 class SessionsController {
     async create(request, response) {
         const { email, password } = request.body;
-        
-        return response.json({ email, password });
+
+        const user = await knex("users").where({ email }).first();
+
+        if(!user) {
+            throw new AppError("Email e/ou senha incorreta.", 401)
+        }
+
+        const passwordMatched = await compare(password, user.password);
+
+        if(!passwordMatched) {
+            throw new AppError("Email e/ou senha incorreta.", 401)
+        }
+
+        return response.json(user);
     }
 }
 

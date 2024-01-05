94fd03e86fd55e1a4830e1ed2b0fe8cef076b619
Fernando Rodrigues <fernando111oficina@gmail.com>
2024-01-05 12:39:25 +0100

    sessions controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,9 +5,11 @@ const routes = Router();
 const usersRouter = require("./users.routes")
 const notesRouter = require("./notes.routes")
 const tagsRouter = require("./tags.routes")
+const sessionsRouter = require("./sessions.routes")
 
 routes.use("/users", usersRouter);
 routes.use("/notes", notesRouter)
 routes.use("/tags", tagsRouter);
+routes.use("/sessions", sessionsRouter);
 
 module.exports = routes;

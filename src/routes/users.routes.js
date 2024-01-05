6abfe0a18187d02c61931b1afa95b078b300e8b9
Fernando Rodrigues <fernando111oficina@gmail.com>
2024-01-05 18:14:57 +0100

    upload img

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,20 @@
 const { Router } = require("express");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router(); // funcionalidade Router de dentro do express
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update)
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+    console.log(request.file.filename);
+    response.json();
+});
 
-module.exports = usersRoutes
+module.exports = usersRoutes;
